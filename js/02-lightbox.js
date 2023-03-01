98bbfa4ddda209d177bf1ed922d511745ac7fccb
YuliiaSliepchenko <slepajuli@gmail.com>
2023-03-01 20:02:25 +0200

    07_2_done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,5 @@
 import { galleryItems } from './gallery-items.js';
-// import galleryItems from "./gallery-items.js";
+
 // Change code below this line
 const galleryEl = document.querySelector(".gallery");
 console.log(galleryEl);
@@ -8,7 +8,7 @@ const createImg = ({preview, original, description})=>`
 <a class="gallery__item" 
 href="${original}">
 <img class="gallery__image"
- src="${preview}{" 
+ src="${preview}" 
  alt="${description}" />
 </a>
 </div>`;
