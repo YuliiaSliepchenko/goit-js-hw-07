d4f6e481a075c2045bf0d42f90b00721922cd21c
YuliiaSliepchenko <slepajuli@gmail.com>
2023-03-01 12:57:23 +0200

    7_2Update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
+// import galleryItems from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+const createImg = ({preview, original, description})=>`
+<div class="gallery__item">
+<a class="gallery__item" 
+href="${original}">
+<img class="gallery__image"
+ src="${preview}{" 
+ alt="${description}" />
+</a>
+</div>`;
+console.log(createImg);
 
-console.log(galleryItems);
+const galleryElsImg = galleryItems.map(createImg).join("");
+console.log(galleryElsImg);
+
+galleryEl.insertAdjacentHTML("beforeend",galleryElsImg);
+galleryEl.addEventListener("click",onImgContainer)
+function onImgContainer(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    }
+    console.log(evt.target);
+   
+    new SimpleLightbox(".gallery a", {
+        captionSelector: 'img',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        scrollZoom: false,
+       });}
